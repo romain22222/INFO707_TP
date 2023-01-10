ee2e612ff5b107808b17087e08da66e4755a4b81
Huseyin Yurtseven <yurtseven68210@hotmail.fr>
2023-01-10 21:24:02 +0100

    try find process with pid

--- collector.js
+++ collector.js
@@ -1,8 +1,36 @@
 const { fork } = require("child_process");
-
+const find = require("find-process");
+const ps = require("ps-node");
 
 process.on("message", (message) => {
   if (message.type === "SIGNAL") {
     console.log("I'm collector Received signal from the clock");
+
+    //get variable
+    let variable = message.variable;
+
+    ps.lookup({ pid: message.variable.pid }, function (err, resultList) {
+      if (err) {
+        throw new Error(err);
+      }
+      variable = resultList[0];
+      console.log("ma variable est : ", variable);
+      variable.send({ type: "GET_CAPTEUR", process });
+      variable.on("message", (message) => {
+        if (message.type === "CAPTEUR") {
+          const capteurs = message.capteur;
+          for (let i = 0; i < capteurs.length; i++) {
+            capteurs[i].send({ type: "GIVE ME VALUE", process });
+          }
+        }
+      });
+    });
+
+    //variable.send({ type: "GET_CAPTEUR", process });
+  }
+
+  if (message.type === "TEMPERATURE_MEASUREMENT") {
+    console.log("I'm collector Received signal from the capteur");
+    console.log(message.temperature);
   }
 });
--- horloge.js
+++ horloge.js
@@ -1,10 +1,26 @@
 const { fork } = require("child_process");
+const variable = fork("variable.js");
+
+// init all processus in variable.js
 
 const collectors = [];
 for (let i = 0; i < 3; i++) {
   collectors[i] = fork("collector.js");
+  //send current pid to variable
+  variable.send({ type: "NEW_COLLECTOR", pid: collectors[i] })
 }
 const controller = fork("controller.js");
+variable.send({ type: "NEW_CONTROLLER", pid: controller })
+
+
+const capteurs = [];
+for (let i = 0; i < 3; i++) {
+  capteurs[i] = fork("capteur.js");
+  //send current pid to variable
+  variable.send({ type: "NEW_CAPTEUR", pid: capteurs[i] });
+}
+
+// init fini
 
 // Periodically send a signal to the collectors and controller
 const delta_1 = 1000; // Replace 1000 with the desired value of delta_1 in milliseconds
@@ -12,8 +28,8 @@ setInterval(() => {
     console.log("Sending signal to the collectors and controller")
   for (let i = 0; i < 3; i++) {
     // Send a signal to the collector
-    collectors[i].send({ type: "SIGNAL" });
+    collectors[i].send({ type: "SIGNAL", variable });
   }
   // Send a signal to the controller
-  controller.send({ type: "SIGNAL" });
+  controller.send({ type: "SIGNAL", variable });
 }, delta_1);
