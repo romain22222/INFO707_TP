47d0dc161e4d79c6acd711e7fb2b92f9b3933ca2
Paul Delifer <paul.delifer@yahoo.fr>
2023-01-11 20:59:51 +0100

    qsqs

--- controller.js
+++
@@ -1,7 +0,0 @@
-const { fork } = require("child_process");
-
-process.on("message", (message) => {
-  if (message.type === "SIGNAL") {
-    console.log("I'm controller Received signal from the clock");
-  }
-});
